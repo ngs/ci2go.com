08a8ef6c2017201e1c455ba232339377fe4d01cb
Atsushi Nagase <a@ngs.io>
2014-11-12 17:18:04 +0900

    Setup website

--- Gemfile
+++ Gemfile
@@ -1,16 +1,13 @@
-# If you have OpenSSL installed, we recommend updating
-# the following line to use "https"
 source 'http://rubygems.org'
 gem 'dotenv'
 
 gem "middleman", "~>3.3.7"
 gem "slim", "~> 2.0"
+gem 'sass'
+gem 'coffee-script'
 
-# Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
+gem "middleman-google-analytics"
+gem 'middleman-deploy'
 
-# For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
-
-# Windows does not come with time zone data
-gem "tzinfo-data", platforms: [:mswin, :mingw]
+gem 'middleman-ogp'
--- config.rb
+++ config.rb
@@ -1,77 +1,77 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-###
-# Gem
-###
 require 'slim'
+require 'dotenv'
+
+Dotenv.load
+
+cname = 'ci2go.com'
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
+
+activate :ogp do |ogp|
+  ogp.namespaces = {
+    fb: {
+      admins: '1034163350',
+      app_id: '389393831218391'
+    },
+    og: {
+      title: 'CI2Go The CircleCI Client for iPhone and iPad',
+      site_name: 'CI2Go',
+      locale: 'en_US',
+      type: 'website',
+      description: 'CI2Go is a CircleCI Client for iPhone and iPad.',
+      image: {
+        '' => "http://#{cname}/images/og.png",
+        width: 1200,
+        height: 630
+      }
+    }
+  }
+  ogp.base_url = "http://#{cname}/"
+end
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+  ignore '.DS_Store'
+  ignore '.*.swp'
+  ignore '_drafts'
+  ignore 'redirect.html'
 
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :favicon_maker, :icons => {
+    "favicon-hires.png" => [
+      { icon: "apple-touch-icon-152x152-precomposed.png" },
+      { icon: "apple-touch-icon-144x144-precomposed.png" },
+      { icon: "apple-touch-icon-120x120-precomposed.png" },
+      { icon: "apple-touch-icon-114x114-precomposed.png" },
+      { icon: "apple-touch-icon-76x76-precomposed.png" },
+      { icon: "apple-touch-icon-72x72-precomposed.png" },
+      { icon: "apple-touch-icon-60x60-precomposed.png" },
+      { icon: "apple-touch-icon-57x57-precomposed.png" },
+      { icon: "apple-touch-icon-precomposed.png", size: "57x57" },
+      { icon: "apple-touch-icon.png", size: "57x57" },
+      { icon: "favicon-196x196.png" },
+      { icon: "favicon-160x160.png" },
+      { icon: "favicon-96x96.png" },
+      { icon: "mstile-144x144", format: "png" }
+    ],
+    "favicon-lores.png" => [
+      { icon: "favicon-32x32.png" },
+      { icon: "favicon-16x16.png" },
+      { icon: "favicon.png", size: "16x16" },
+      { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" }
+    ]
+  }
 
-  # Enable cache buster
-  # activate :asset_hash
+end
 
-  # Use relative URLs
-  # activate :relative_assets
+activate :google_analytics do |ga|
+  ga.tracking_id = 'UA-56666052-2'
+end
 
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+activate :deploy do |deploy|
+  IO.write "source/CNAME", cname
+  deploy.method = :git
+  deploy.branch = 'gh-pages'
+  deploy.remote = "https://#{ENV['GH_TOKEN']}@github.com/ngs/#{cname}.git"
 end
